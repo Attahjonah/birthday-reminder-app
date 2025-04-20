d3260c15803f99268de882faad9553bb1452a91a
Jonah Attah <attahjonah93@gmail.com>
2025-04-20 11:15:45 +0100

    res.sendFile

--- app.js
+++ app.js
@@ -18,5 +18,9 @@ connectDB();
 app.use('/api/users', userRoutes);
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/register.html");
+  });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
